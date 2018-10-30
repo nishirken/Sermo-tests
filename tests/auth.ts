8973dd7b9a3778e1e2e693d77c5693b192804d65
dskurikhin <d.skurikhin@eastwind.ru>
2018-10-30 18:13:21 +0500

    add screens verification

--- tests/auth.ts
+++ tests/auth.ts
@@ -1,18 +1,19 @@
 import { hostName, testPassword, testEmail } from '../config';
+import { screenshotVerification } from '../utils';
 
 describe('Auth', () => {
   it('Show login', async () => {
     await page.goto(`${hostName}/src/Main.elm`);
     await page.click('[data-e2e="login-link"]');
     await page.waitFor('[data-e2e="login-page"]');
-    await page.screenshot({ path: 'screenshots/login-page.png' });
+    await expect(screenshotVerification('login-page.png')).resolves.toBeTruthy();
   });
 
   it('Show signin', async () => {
     await page.goto(`${hostName}/src/Main.elm`);
     await page.click('[data-e2e="signin-link"]');
     await page.waitFor('[data-e2e="signin-page"]');
-    await page.screenshot({ path: 'screenshots/signin-page.png' });
+		await expect(screenshotVerification('signin-page.png')).resolves.toBeTruthy();
   });
 
   it('Login and enter inside app', async () => {
@@ -22,6 +23,6 @@ describe('Auth', () => {
     await page.type('[data-e2e="login-password"]', testPassword);
     await page.click('[data-e2e="login-submit"]');
     await page.waitFor('[data-e2e="app"]');
-    await page.screenshot({ path: 'screenshots/a-page.png' });
+		await expect(screenshotVerification('app-page.png')).resolves.toBeTruthy();
   });
 });
--- utils.ts
+++ utils.ts
@@ -1,36 +1,40 @@
-import { Page } from 'puppeteer';
 import { pathExists, mkdir } from 'fs-extra';
 import { join } from 'path';
 import looksSame from 'looks-same';
 
 const screenshotsFolder = 'screenshots';
-export const screenshotVerification = async (page: Page, screenshotName: string): Promise<boolean> => {
-	const originalPath = join(process.cwd(), screenshotsFolder, screenshotName);
-	const isExists = await pathExists(originalPath);
+export const screenshotVerification = async (screenshotName: string): Promise<boolean | void> => {
+	try {
+		const originalPath = join(process.cwd(), screenshotsFolder, screenshotName);
+		const isExists = await pathExists(originalPath);
 
-	if (!isExists) {
-		await page.screenshot({ path: originalPath, });
-		return true;
+		if (!isExists) {
+			await page.screenshot({ path: originalPath, });
+			return true;
+		}
+
+		const tmpFolder = join(process.cwd(), screenshotsFolder, 'tmp');
+		const isTmpExists = await pathExists(tmpFolder);
+		if (!isTmpExists) {
+			await mkdir(tmpFolder);
+		}
+
+		const tmpPath = join(tmpFolder, screenshotName);
+		await page.screenshot({ path: tmpPath });
+
+		const res = await (
+			new Promise<boolean>((resolve, reject) => {
+				looksSame(originalPath, tmpPath, (error, equal) => {
+					if (error) {
+						reject(error);
+					}
+					resolve(equal);
+				});
+			})
+		);
+
+		return res;
+	} catch (error) {
+		console.error('Error with screenshot verification', error);
 	}
-
-	const isTmpExists = await pathExists(join(process.cwd(), screenshotsFolder, 'tmp'));
-	if (!isTmpExists) {
-		await mkdir(join(process.cwd(), screenshotsFolder, 'tmp'));
-	}
-
-	const tmpPath = join(process.cwd(), screenshotsFolder, 'tmp', screenshotName);
-	await page.screenshot({ path: tmpPath });
-
-	const res = await (
-		new Promise<boolean>((resolve, reject) => {
-			looksSame(originalPath, tmpPath, (error, equal) => {
-				if (error) {
-					reject(error);
-				}
-				resolve(equal);
-			});
-		})
-	);
-
-	return res;
 };
